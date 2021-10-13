2ead2ab78fb0db9893a75d4ec68b51dbfb68de71
Naishadh Shroff <naishadh.shroff@gmail.com>
2021-10-13 19:04:10 -0400

    removed ngModel error

--- DataBinding/src/app/app.module.ts
+++ DataBinding/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
@@ -12,7 +12,8 @@ import { ServerElementComponent } from './server-element/server-element.componen
     ServerElementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

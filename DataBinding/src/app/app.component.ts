e774ad8a33012b47a7f1f41bcd6588648b899e5c
Naishadh Shroff <naishadh.shroff@gmail.com>
2021-10-16 12:12:47 -0400

    new project added

--- DataBinding/src/app/app.component.ts
+++ DataBinding/src/app/app.component.ts
@@ -6,22 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type:'server',name:'test server', content:'just a test !'}];
-  title = 'DataBinding';
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
 
-  onServerAdded(serverData: {serverName:string,serverContent:string}) {
+  onAddServer() {
     this.serverElements.push({
       type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
+      name: this.newServerName,
+      content: this.newServerContent
     });
   }
 
-  onBlueprintAdded(blueprintData: { serverName: string,serverContent: string }) {
+  onAddBlueprint() {
     this.serverElements.push({
       type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent
+      name: this.newServerName,
+      content: this.newServerContent
     });
   }
 }
